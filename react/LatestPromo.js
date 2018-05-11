2ddc60d6038a0fd0ab571ca788be10b658694d43
Tiago Gimenes <tlgimenes@gmail.com>
2018-05-11 15:15:09 -0300

    initial commit

---
+++ react/LatestPromo.js
@@ -0,0 +1,11 @@
+import {Component} from 'react'
+
+export default class LatestPromo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World 2123123s</h1>
+            </div>
+        )
+    }
+}
