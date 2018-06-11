ff946d3de46bf10010844f36e403185da92f9b13
Victor Elias <victorgelias@gmail.com>
2018-06-11 12:21:31 +0200

    Prepare for demo

--- react/nested_components/product.js
+++ react/nested_components/product.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types'
 import {ExtensionPoint} from 'render'
 import Card from '@vtex/styleguide/lib/Card'
 
+const productLink = (product) => `https://dreamstore.myvtexdev.com/${product.linkText}/p`
+
 // We can make any architecture we wish using default React
 export class Product extends Component {
   render() {
-    const {product: {productName, productId}} = this.props
+    const {product} = this.props
     return (
-      <Card>{productName}: {productName}</Card>
+      <Card>{product.productName}</Card>
     )
   }
 }
